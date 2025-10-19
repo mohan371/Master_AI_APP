aa682baee0d73c5dba43688426c3de40ceadbe7e
Mohan N V <shivamohan899@gmail.com>
2025-10-19 10:35:33 +0530

    Restore cleaned keys.php without secret

---
+++ backend/config/keys.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+define('HF_API_KEY', $_ENV['HUGGINGFACE_API_KEY']);
